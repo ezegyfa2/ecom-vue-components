96c276b8a9700803fafaf6bea18889be479c54dc
Darvas Istvan <darvasvok@gmail.com>
2023-02-26 16:44:01 +0200

    Making code pretty

--- src/ProductsPage/slider/Component.vue
+++ src/ProductsPage/slider/Component.vue
@@ -1,43 +1,43 @@
 <template lang="pug">
-  include Template.pug
+  	include Template.pug
 </template>
 
 <script>
-import vueSlider from 'vue-slider-component'
+	import vueSlider from 'vue-slider-component'
 
-export default {
-	name: 'slider',
-	components: {
-		vueSlider
-	},
-	data() {
-		return {
-			value: [
-				36,
-				756
-			],
-			options: {
-				width: "auto",
-				height: 4,
-				dotSize: 10,
-				min: 36,
-				max: 756,
-				show: true,
-				useKeyboard: true,
-				tooltip: "hover",
-				formatter: (v) => `$${v}`,
-				mergeFormatter: (v1, v2) => `$${v1} ~ $${v2}`,
-				processStyle: {
-					"backgroundColor": "#999"
+	export default {
+		name: 'slider',
+		components: {
+			vueSlider
+		},
+		data() {
+			return {
+				value: [
+					36,
+					756
+				],
+				options: {
+					width: "auto",
+					height: 4,
+					dotSize: 10,
+					min: 36,
+					max: 756,
+					show: true,
+					useKeyboard: true,
+					tooltip: "hover",
+					formatter: (v) => `$${v}`,
+					mergeFormatter: (v1, v2) => `$${v1} ~ $${v2}`,
+					processStyle: {
+						"backgroundColor": "#999"
+					}
 				}
 			}
-		}
-	},
-	methods: {
-		getValue(name) {
-			let slider = this.$refs[name]
-			this.$emit('clicked', slider.getValue())
+		},
+		methods: {
+			getValue(name) {
+				let slider = this.$refs[name]
+				this.$emit('clicked', slider.getValue())
+			}
 		}
 	}
-}
 </script>
