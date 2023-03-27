2f5bc251f8d2b4ebb644b786081c80bf19af8692
József Bagota <66431498+bagotaj@users.noreply.github.com>
2023-03-27 18:48:46 +0200

    creating new input component

---
+++ src/FormItems/Inputs/Email/Component.vue
@@ -0,0 +1,13 @@
+<script>
+    import EmailInput from './../../../../../helper-vue-components/src/Form/BootstrapFormItems/FormItems/Inputs/Email/Component.vue'
+
+    export default {
+        mixins: [
+            EmailInput,
+        ]
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "../formItem.scss";
+</style>
