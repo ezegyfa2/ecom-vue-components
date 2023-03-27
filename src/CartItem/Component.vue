377c0fac7dbe836f19944d5e044efd9755074fc4
József Bagota <66431498+bagotaj@users.noreply.github.com>
2023-03-27 16:31:02 +0200

    making CartItem component

---
+++ src/CartItem/Component.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+	include Template.pug
+</template>
+
+<script>
+export default {
+	props: {
+		image_url: {
+				type: String
+		},
+		image_alt: {
+				type: String,
+				default() {
+						return ''
+				}
+		},
+		title: {
+			type: String,
+		},
+		price: {
+			type: String,
+		},
+		id: {
+			type: String,
+		},
+	},
+	computed: {
+		removeThing(id) {
+			this.$store.commit('outCart',id)
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+	@import "Style.scss";
+</style>
