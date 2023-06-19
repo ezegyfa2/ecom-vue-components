5a7f4fe74cfa3c715cfa14941818a6e96375fbae
József Bagota <jozsef.bagota@gmail.com>
2023-06-19 21:00:36 +0200

    creating ikonlinkswithimage component.

---
+++ src/IconLinks/IconLinksWithImage/Component.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+    include Template.pug
+</template>
+
+<script>
+    import IconWithRightText from '../../../../helper-vue-components/src/IconLinks/IconWithRightText/Component.vue'
+
+    export default {
+        mixins: [
+            IconWithRightText
+        ],
+        props: {
+            img_url: {
+                type: String
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+  	@import "Style.scss";
+</style>
