564051d1a7b2fafd738d804ea9b10e0ead1f4e99
ezegyfa2 <darvasvok@gmail.com>
2023-08-08 12:33:35 +0300

    Changing registration template

--- src/Templates/registration.js
+++ src/Templates/registration.js
@@ -3,20 +3,18 @@ export default {
     params: {
         content_sections: [
             {
-                type: 'ecom-form',
+                type: 'centered-content',
                 data: {
-                    formitem_sections: '-++form_item_sections',
-                    button_section: {
-                        type: 'ecom-submit-button',
-                        data: {
-                            submit_item: 'submit',
-                            submit_label: 'Submit'
+                    content_sections: [
+                        {
+                            type: 'ecom-form',
+                            data: {
+                                form_item_sections: "-++form_item_sections",
+                                title: 'Registration',
+                                button_title: 'Registration'
+                            }
                         }
-                    },
-                    title: 'Registration',
-                    link_title: 'Login',
-                    url: '/login',
-                    text: 'I have account:'
+                    ]
                 }
             }
         ]
