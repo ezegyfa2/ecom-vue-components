94265a4ffd7e9359939d8e02d44f1f1a901f4886
József Bagota <jozsef.bagota@gmail.com>
2023-08-24 14:25:53 +0200

    adding related item setter component

--- src/Templates/admin.js
+++ src/Templates/admin.js
@@ -1,18 +1,25 @@
 export default {
-    template_type_name: 'ecom_layout2',
+    template_type_name: 'ecom_layout',
     params: {
         content_sections: [
             {
-                type: 'ecom-admin',
+                type: 'ecom-related-item-setter',
                 data: {
-                    delete_title: 'Delete',
-                    admin_title: 'Admin',
-                    images: ['image1','image2','image3','image4','image5','image6','image7','image8'],
+                    title: 'Images',
+                    name: 'images',
+                    related_items: ['image1','image2','image3','image4','image5','image6','image7','image8'],
                     button_section: {
                         type: 'ecom-form-button',
                         data: {
                             content: 'Add',
                             is_submit_button: true
+                        }
+                    },
+                    delete_button_section: {
+                        type: 'ecom-icon-link-with-image',
+                        data: {
+                            img_url: 'images/trash-can-regular.svg',
+                            img_alt: ''
                         }
                     }
                 }
